241fd1dac999d17b661c41e658c7a68472742edd
tuguldur <aa402115@gmail.com>
2021-05-26 15:59:30 +0800

    db

--- include/initialize.php
+++ include/initialize.php
@@ -6,9 +6,9 @@
 //(\ for windows, / for Unix)
 defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
 
-defined('SITE_ROOT') ? null : define ('SITE_ROOT', $_SERVER['DOCUMENT_ROOT'].DS.'E-LearningSystem');
+defined('SITE_ROOT') ? null : define('SITE_ROOT', $_SERVER['DOCUMENT_ROOT'].DS.'');
 
-defined('LIB_PATH') ? null : define ('LIB_PATH',SITE_ROOT.DS.'include');
+defined('LIB_PATH') ? null : define('LIB_PATH',SITE_ROOT.DS.'include');
 
 //load the database configuration first.
 require_once(LIB_PATH.DS."config.php");
